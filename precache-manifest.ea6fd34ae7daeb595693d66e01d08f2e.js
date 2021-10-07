4ff09fb7c413897e7721f74860ba222cb0cc2dc0
Untapped <untapgaming@gmail.com>
2021-10-07 16:53:32 -0400

    Updates

---
+++ precache-manifest.ea6fd34ae7daeb595693d66e01d08f2e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e576b31e46b7b562577cb818efc46dc",
+    "url": "/Un-Tapped/SkillTree/index.html"
+  },
+  {
+    "revision": "46d814f276fd81b26d66",
+    "url": "/Un-Tapped/SkillTree/static/css/main.c139c031.chunk.css"
+  },
+  {
+    "revision": "d9ef64bba6fdafe7d6b4",
+    "url": "/Un-Tapped/SkillTree/static/js/2.9e0341c2.chunk.js"
+  },
+  {
+    "revision": "46d814f276fd81b26d66",
+    "url": "/Un-Tapped/SkillTree/static/js/main.49f9636c.chunk.js"
+  },
+  {
+    "revision": "d322d6850d7c497ad487",
+    "url": "/Un-Tapped/SkillTree/static/js/runtime~main.60ae79f6.js"
+  },
+  {
+    "revision": "cfcc7caf241e04cd2062d25b6dda7300",
+    "url": "/Un-Tapped/SkillTree/static/media/Basic_Shield_Projection.cfcc7caf.webp"
+  },
+  {
+    "revision": "9ad95b70ba12f11684866fa50925e37d",
+    "url": "/Un-Tapped/SkillTree/static/media/Beam_Amplification.9ad95b70.webp"
+  },
+  {
+    "revision": "bb68e2bd2a6cca067c9ece95cc21c555",
+    "url": "/Un-Tapped/SkillTree/static/media/Burst_Modulated_Laser.bb68e2bd.webp"
+  },
+  {
+    "revision": "9648e94ebe343549aaa3fa4e083865ac",
+    "url": "/Un-Tapped/SkillTree/static/media/High_Intensity_Plasma.9648e94e.webp"
+  },
+  {
+    "revision": "3063255b1a9fd5884d7b44bbb9650cd4",
+    "url": "/Un-Tapped/SkillTree/static/media/ImprovedContainment.3063255b.webp"
+  },
+  {
+    "revision": "45929413873a8036e563053050e55992",
+    "url": "/Un-Tapped/SkillTree/static/media/Improved_Beam_Emitters.45929413.webp"
+  },
+  {
+    "revision": "a7213f190414c3f2368f7554e8c87738",
+    "url": "/Un-Tapped/SkillTree/static/media/Inspired_Reactor_Design.a7213f19.webp"
+  },
+  {
+    "revision": "f06d88e1bc0047cb985918b5c075c2bc",
+    "url": "/Un-Tapped/SkillTree/static/media/Potent_Shield_Generation.f06d88e1.webp"
+  },
+  {
+    "revision": "95497baba992d896013e470107a6e729",
+    "url": "/Un-Tapped/SkillTree/static/media/Precision_Beam_Focus.95497bab.webp"
+  },
+  {
+    "revision": "c38f1d973f5fc5c69e5763df8bcbe6a8",
+    "url": "/Un-Tapped/SkillTree/static/media/PsiTech_Powered_lasers.c38f1d97.webp"
+  },
+  {
+    "revision": "d7875e2b49699a02277859d587940289",
+    "url": "/Un-Tapped/SkillTree/static/media/Psitech_Shield_Emitters.d7875e2b.webp"
+  },
+  {
+    "revision": "1b77bd0861253bf7547e0a7dccf1f948",
+    "url": "/Un-Tapped/SkillTree/static/media/Quick_Reacting_Cycling.1b77bd08.webp"
+  },
+  {
+    "revision": "53309a2e0dcdfe220e02d176b39950a0",
+    "url": "/Un-Tapped/SkillTree/static/media/Raised_Shield_Harmonics.53309a2e.webp"
+  },
+  {
+    "revision": "2fc7c45ac27dd311558940d04a850e82",
+    "url": "/Un-Tapped/SkillTree/static/media/Rapid_Plasma_Generation.2fc7c45a.webp"
+  },
+  {
+    "revision": "bface4183da737ac32b801f5b0c8d832",
+    "url": "/Un-Tapped/SkillTree/static/media/Refined_Laser_Cannon.bface418.webp"
+  },
+  {
+    "revision": "cab5820e084572f68042ccc9b3a06bab",
+    "url": "/Un-Tapped/SkillTree/static/media/Strong_Plasma_Ejection.cab5820e.webp"
+  },
+  {
+    "revision": "5fcf695d23c8891eba46e0357a2f13ed",
+    "url": "/Un-Tapped/SkillTree/static/media/Superior_Emission_Coils.5fcf695d.webp"
+  },
+  {
+    "revision": "5b9149b7071a76a12bee1d50f782bdb9",
+    "url": "/Un-Tapped/SkillTree/static/media/Synergetic_Reactions.5b9149b7.webp"
+  },
+  {
+    "revision": "ba3bc9aea9d6a1ff12215a7a9cc6ac7a",
+    "url": "/Un-Tapped/SkillTree/static/media/logo.ba3bc9ae.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.704cdd51f634da2d7b08def0c5e9ee3a.js"
+  "/Un-Tapped/SkillTree/precache-manifest.ea6fd34ae7daeb595693d66e01d08f2e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Un-Tapped/SkillTree/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
